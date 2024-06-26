8c6d3f56256b7da70225c4e67f78b24828f1125c
Vlad Constantinescu <vlad.constantinescu@totaltech.ro>
2024-06-26 22:41:11 +0300

    role in loadouts

--- models/character.py
+++ models/character.py
@@ -293,6 +293,7 @@ class SwtorLoadout(models.Model):
     loadout_url = fields.Char(string='Loadout URL', store=True)
     loadout_iframe = fields.Html(string='Parsely Loadout', compute='_compute_loadout_iframe', store=True, sanitize=False)
     notes = fields.Html(string='Notes')
+    role_ids = fields.Many2one('swtor.role', string='Roles', store=True, domain="[('id', 'in', character_id.role_ids)]")
 
     @api.depends('loadout_url')
     def _compute_loadout_iframe(self):
